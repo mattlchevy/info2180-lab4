b0f04f97bfc48f5d58265d4b40741929b4a21234
Matthew Chevannes <lear.chevy@hotmail.com>
2020-11-10 22:28:15 -0500

    help

--- apps.js
+++ apps.js
@@ -1,13 +1,37 @@
-$(document).ready(function(){
-    var goheros = $('hello');
+window.onload = function(){
+    button = document.getElementById("hello")
+    result = document.getElementById("result")
+    var httpRequest
 
-    goheros.on('click', function(){
-        $.ajax('superheroes.php', {
-            method: 'GET'
-        }).done(function(response){
-          alert(response);
-        }).fail(function(){
-            alert('Error with request...');
-        });
-    });
-});
+
+
+    button.addEventListener("click", function(e){
+        e.preventDefault();
+
+        httpRequest = new XMLHttpRequest();
+
+        //GET
+        
+        var url = "superheroes.php";
+        httpRequest.onload = function(){
+           result.innerHTML = this.response
+        }
+        httpRequest.onreadystatechange = Hello
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    })
+    
+    function Hello(){
+        if(httpRequest.readystate === XMLHttpRequest.DONE){
+          if (httpRequest.status === 200){
+              var response = httpRequest.responseText
+          }else{
+              alert('There is an error');
+          }
+        }
+    }
+
+
+    
+};
+
--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Example</title>
         <script src="apps.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-        </script>
+        <script
+			  src="https://code.jquery.com/jquery-3.5.1.min.js"
+			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+			  crossorigin="anonymous"></script>
     </head>
     <body>
         <h2>Avengers Character Search</h2>
-        <form class="heroes" id="heroes" >
-            <button type="submit" id="hello">Search !</button>
+        <form action="" method="post">
+            <input id="search" placeholder="choose any superhero ...">
+        <button type="button" class="btn" id="hello">Search</button>
         </form>
+        <br>
+        <h2>Result</h2>
+        <div id="result"></div>
     </body>
 </html>
